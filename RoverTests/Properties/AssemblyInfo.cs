ec43ce9d909e0012e1e8b9fe522db6276a448704
ravi-km <raves00@gmail.com>
2018-07-23 15:31:54 +0200

    Add files via upload

---
+++ RoverTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RoverTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RoverTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("534b00b4-8dab-42f2-8c6a-1fe34aec4428")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ RoverTests/RoverTests.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TheUltimateMARSRover.Contracts;
+
+namespace RoverTests
+{
+    [TestClass]
+    public class RoverTests
+    {
+        public static UltimateRover Rover = new UltimateRover();
+
+        public static void InitRover()
+        {
+            Rover = new UltimateRover
+            {
+                CurrPosition = new Position { X = 1, Y = 1 },
+                Landscape = new Position { X = 10, Y = 10 },
+                Orientation = Orientation.N
+            };
+        }
+
+        [TestMethod]
+        public void RoverTurnLeft()
+        {
+            InitRover();
+            Rover.TurnLeft(1);
+            Assert.AreEqual(Orientation.W, Rover.Orientation);
+        }
+
+        [TestMethod]
+        public void RoverTurnRight()
+        {
+            InitRover();
+            Rover.TurnRight(1);
+            Assert.AreEqual(Orientation.E, Rover.Orientation);
+        }
+
+        [TestMethod]
+        public void RoverMoveOneStep()
+        {
+            InitRover();
+            Rover.Move(1);
+            Assert.AreEqual(2, Rover.CurrPosition.Y);
+        }
+
+        [TestMethod]
+        public void RoverMoveTwoSteps()
+        {
+            InitRover();
+            Rover.Move(2);
+            Assert.AreEqual(3, Rover.CurrPosition.Y);
+        }
+
+
+        [TestMethod]
+        public void RoverTurnLeftTwice()
+        {
+            InitRover();
+            Rover.TurnLeft(2);
+            Assert.AreEqual(Orientation.S, Rover.Orientation);
+        }
+
+        [TestMethod]
+        public void RoverTurnRightTwice()
+        {
+            InitRover();
+            Rover.TurnRight(2);
+            Assert.AreEqual(Orientation.S, Rover.Orientation);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void RoverOutOfBound()
+        {
+            InitRover();
+            Rover.Move(20);
+        }
+
+    }
+}
